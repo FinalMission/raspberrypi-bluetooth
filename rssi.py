f0c0aa75331bf42afa81ed8efdc1d100e361f51a
csyouk <csyouk@hanmail.net>
2017-12-04 21:24:20 +0900

    update documents

--- rssi.py
+++ rssi.py
@@ -4,5 +4,5 @@ import time
 CS_MAC = 'F4:0F:24:29:73:C0'
 
 while(1):
-    subprocess.call('hcitool rssi {0}'.format(CS-MAC), shell=True)
+    subprocess.call('hcitool rssi {0}'.format(CS_MAC), shell=True)
     time.sleep(1)
