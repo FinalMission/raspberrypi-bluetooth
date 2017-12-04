d3be53c172989f0c2da2ccfc799555d0a2abac93
csyouk <csyouk@hanmail.net>
2017-12-04 21:32:57 +0900

    update documents

--- rssi.py
+++ rssi.py
@@ -4,12 +4,12 @@ import sys
 
 CS_MAC = 'F4:0F:24:29:73:C0'
 INTERVAL = 1
-sys.argv[1] = INTERVAL
 
-if(sys.argv[1]):
+try:
+    sys.argv[1]
     INTERVAL = int(sys.argv[1])
-
-print("Running {0} with interval {1}".format(sys.argv[0], INTERVAL))
+except: pass
+print("Running {0} with interval {1} sec".format(sys.argv[0], INTERVAL))
 
 while(1):
     subprocess.call('hcitool rssi {0}'.format(CS_MAC), shell=True)
