cd37595c9499319184e15c967cd49bdee2ad5034
csyouk <csyouk@hanmail.net>
2017-12-04 21:28:53 +0900

    update documents

--- rssi.py
+++ rssi.py
@@ -5,8 +5,9 @@ import sys
 CS_MAC = 'F4:0F:24:29:73:C0'
 INTERVAL = 1
 
-if(sys.argv[2]):
-    INTERVAL = int(sys.argv[2])
+print(sys.argv)
+if(sys.argv[1]):
+    INTERVAL = int(sys.argv[1])
 
 while(1):
     subprocess.call('hcitool rssi {0}'.format(CS_MAC), shell=True)
