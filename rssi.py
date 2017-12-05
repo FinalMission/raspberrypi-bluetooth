91b1f120ea31c6a944eac4ef43b2cd684e6d15c8
csyouk <csyouk@hanmail.net>
2017-12-05 09:41:30 +0900

    update DJ's ble addr

--- rssi.py
+++ rssi.py
@@ -3,6 +3,7 @@ import time
 import sys
 
 CS_MAC = 'F4:0F:24:29:73:C0'
+DJ_S8_PLUS = "94:8B:C1:5C:5D:17"
 INTERVAL = 1
 
 try:
@@ -12,5 +13,5 @@ except: pass
 print("Running {0} with interval {1} sec".format(sys.argv[0], INTERVAL))
 
 while(1):
-    subprocess.call('hcitool rssi {0}'.format(CS_MAC), shell=True)
+    subprocess.call('hcitool rssi {0}'.format(DJ_S8_PLUS), shell=True)
     time.sleep(INTERVAL)
