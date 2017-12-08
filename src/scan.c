c122a0b151d24316e424fd0b264eebc73d4fa890
csyouk <csyouk@hanmail.net>
2017-12-08 20:37:04 +0900

    correct errata

--- src/scan.c
+++ src/scan.c
@@ -35,7 +35,8 @@ void print_ble_info(le_advertising_info * info){
 	printf("ble device addr : %s\n", addr);
 	printf("ble length : %d\n", (int)info->length + 1);
 	printf("ble tx power : 0x%02X\n", (int)info->data[info->length]);
-	printf("ble rssi %d\n", (int)(info->data[info->length] | 0xffffff00))
+	printf("ble rssi %d\n", (int)(info->data[info->length] | 0xffffff00));
+	
 	printf("info->data : ");
 	int len=(int)info->length + 1;
 	int index=0;
