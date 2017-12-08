ac5faf35b3678b288aeaad3cdbda1b94c72008e4
csyouk <csyouk@hanmail.net>
2017-12-08 20:32:55 +0900

    change ble ad len

--- src/scan.c
+++ src/scan.c
@@ -33,7 +33,7 @@ void print_ble_info(le_advertising_info * info){
 	printf("event type : %d\n", (int)info->evt_type);
 	printf("ble device type : %d\n", (int)info->bdaddr_type);
 	printf("ble device addr : %s\n", addr);
-	printf("ble length : %d\n", (int)info->length);
+	printf("ble length : %d\n", (int)info->length + 1);
 	printf("ble tx power : %d(0x%02X)\n", (int)(info->data[info->length] | 0xffffff00), (int)info->data[info->length]);
 
 	printf("info->data : ");
