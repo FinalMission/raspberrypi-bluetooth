6ad9d412911eae3aeee57844995409d38243eadf
Sunbin <binsong210@gmail.com>
2017-12-09 04:56:19 +0000

    update documents

--- src/shm.h
+++ src/shm.h
@@ -4,10 +4,17 @@
 #define TIMEOUT 30
 #define MAX_BUF 64
 #define ERROR -1
+#define SEG1 0xffeeddcc
+#define SEG2 0xbbaa9988
+#define SEG3 0x77665544
+#define PI0_SEG4 0x33221199
+#define PI1_O_SEG4 0x332211ff
+#define PI1_X_SEG4 0x33221100
+
 typedef struct 
 {
   char prefix[9];
-  unsigned long long uuid[2];
+  unsigned int uuid[4];
   unsigned short major;
   unsigned short minor;
   char tx_power;
